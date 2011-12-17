a3cf12e23a74bd59531df0b7899c30e9d9b04752
Tariqul Islam <tareq@webkutir.net>
2011-12-17 17:12:29 +0600

    V2 URI Changed...

--- github.rb
+++ github.rb
@@ -11,7 +11,7 @@ end
 
 class GitHub
   include HTTParty
-  base_uri "https://github.com/api/v2/json"
+  base_uri "http://github.com/api/v2/json"
 
   def initialize(repo, user=nil, pass=nil)
     @user = user
